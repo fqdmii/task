7b3ee85a73dcdc5bd7eae6d222f0ab3ae6a1f7d6
Ｍｉｚｕｋｉ Ｆｕｋｕｄａ < >
2016-06-13 12:24:07 +0800

    最初のコミット

---
+++ javascript/PHIQ/phiq_top.js
@@ -0,0 +1,37 @@
+
+
+function slideSwitch(){
+	var $active = $('#slideshow p.active');
+
+	if($active.length == 0) $active = $('#slideshow p:last');
+
+	var $next = $active.next().length ? $active.next()
+	: $('#slideshow p:first');
+
+	$active.addClass('last-active');
+
+	$next.css({opacity: 0.0})
+		.addClass('active')
+		.animate({opacity:1.0}, 3000, function()
+			{
+				$active.removeClass('active last-active');
+			});
+}
+
+
+	setInterval(slideSwitch, 6000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
